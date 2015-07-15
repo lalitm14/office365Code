f762b960fc3fea32c9a922ed0bdff07ce9e00fc2
lalitm14 <lalitmohan14@outlook.com>
2015-07-15 18:29:30 +0800

    office 365 projects
    office 365 projects

---
+++ OfficeAuth/OfficeAuth/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using O365_WebApp_SingleTenant.Utils;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OfficeAuth.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+        public void SignOut()
+        {
+            string callbackUrl = Url.Action("SignOutCallback", "Account", routeValues: null, protocol: Request.Url.Scheme);
+
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                new AuthenticationProperties { RedirectUri = callbackUrl },
+                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+
+        public ActionResult SignOutCallback()
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Redirect to home page if the user is authenticated.
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ OfficeAuth/OfficeAuth/Controllers/ContactsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Office365.Discovery;
+using Microsoft.Office365.OutlookServices;
+using O365_WebApp_SingleTenant.Models;
+using O365_WebApp_SingleTenant.Utils;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using OfficeAuth.Models;
+
+
+namespace OfficeAuth.Controllers
+{
+    [Authorize]
+
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public async Task<ActionResult> Index()
+        {
+
+            List<MyContact> myContacts = new List<MyContact>();
+
+            var signInUserId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var userObjectId = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+
+            AuthenticationContext authContext = new AuthenticationContext(SettingsHelper.Authority, new ADALTokenCache(signInUserId));
+
+            try
+            {
+                DiscoveryClient discClient = new DiscoveryClient(SettingsHelper.DiscoveryServiceEndpointUri,
+                    async () =>
+                    {
+                        var authResult = await authContext.AcquireTokenSilentAsync(SettingsHelper.DiscoveryServiceResourceId, new ClientCredential(SettingsHelper.ClientId, SettingsHelper.AppKey), new UserIdentifier(userObjectId, UserIdentifierType.UniqueId));
+
+                        return authResult.AccessToken;
+                    });
+
+                var dcr = await discClient.DiscoverCapabilityAsync("Contacts");
+
+                OutlookServicesClient exClient = new OutlookServicesClient(dcr.ServiceEndpointUri,
+                    async () =>
+                    {
+                        var authResult = await authContext.AcquireTokenSilentAsync(dcr.ServiceResourceId, new ClientCredential(SettingsHelper.ClientId, SettingsHelper.AppKey), new UserIdentifier(userObjectId, UserIdentifierType.UniqueId));
+
+                        return authResult.AccessToken;
+                    });
+
+                var contactsResult = await exClient.Me.Contacts.ExecuteAsync();
+
+                do
+                {
+                    var contacts = contactsResult.CurrentPage;
+                    foreach (var contact in contacts)
+                    {
+                        myContacts.Add(new MyContact { Name = contact.DisplayName });
+                    }
+
+                    contactsResult = await contactsResult.GetNextPageAsync();
+
+                } while (contactsResult != null);
+            }
+            catch (AdalException exception)
+            {
+                //handle token acquisition failure
+                if (exception.ErrorCode == AdalError.FailedToAcquireTokenSilently)
+                {
+                    authContext.TokenCache.Clear();
+
+                    //handle token acquisition failure
+                }
+            }
+
+
+            return View(myContacts);
+        }
+    }
+}
